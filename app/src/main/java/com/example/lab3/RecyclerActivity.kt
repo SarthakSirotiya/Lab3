a304eb0b86663e9442dc0ddcbf95ecd91a187dac
Humberto Torralba <humberto.torralba72@gmail.com>
2022-09-22 17:28:38 -0500

    end of day push

--- app/src/main/java/com/example/lab3/RecyclerActivity.kt
+++ app/src/main/java/com/example/lab3/RecyclerActivity.kt
@@ -1,11 +1,17 @@
 package com.example.lab3
 
 import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
+import com.example.lab3.model.Muscle
 
 class RecyclerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recycler)
+
+        val textView = findViewById<TextView>(R.id.textView_1)
+        textView.text = intent.getSerializableExtra("Muscle").toString()
     }
 }
