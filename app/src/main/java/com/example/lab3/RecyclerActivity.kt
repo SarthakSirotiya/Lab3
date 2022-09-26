a8ad0b0be73c502bdc0151681e5cf9bebeb8306f
Prime72 <manueltorralba20@gmail.com>
2022-09-25 21:00:47 -0500

    Recycler view working!

--- app/src/main/java/com/example/lab3/RecyclerActivity.kt
+++ app/src/main/java/com/example/lab3/RecyclerActivity.kt
@@ -3,20 +3,57 @@ package com.example.lab3
 import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.widget.TextView
-import com.example.lab3.model.Muscle
 import com.example.lab3.RecyclerActivity as RecyclerActivity1
 import com.example.lab3.adapter.ExerciseCardAdapter
+import com.example.lab3.databinding.ActivityRecyclerBinding
+import com.example.lab3.model.Exercise
+import com.example.lab3.model.Muscle
+import com.example.lab3.data.DataSource
+import com.example.lab3.model.Muscle_Exercises
 
 class RecyclerActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityRecyclerBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_recycler)
+        binding = ActivityRecyclerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val textView = findViewById<TextView>(R.id.textView_1)
-        textView.text = intent.getSerializableExtra("Muscle").toString()
+        binding.recyclerView.adapter = ExerciseCardAdapter(
+            applicationContext,
+            determine_exercises(intent.getSerializableExtra("Muscle") as Muscle)
+        )
 
+        // Specify fixed size to improve performance
+        binding.recyclerView.setHasFixedSize(true)
+
+        // Enable up button for backward navigation
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+    }
+
+    fun determine_exercises(my_muscle: Muscle): Muscle_Exercises {
+
+
+        var my_list = when (my_muscle) {
+            Muscle.TRAPS -> DataSource.chest_exercises // TODO TODO TODO
+            Muscle.CHEST -> DataSource.chest_exercises
+            Muscle.BACK -> DataSource.chest_exercises // TODO TODO TODO
+            Muscle.DELTS -> DataSource.delt_exercises
+            Muscle.BICEPS -> DataSource.bicep_exercises
+            Muscle.TRICEPS -> DataSource.tricep_exercises
+            Muscle.FOREARMS -> DataSource.forearm_exercises
+            Muscle.ABS -> DataSource.chest_exercises // TODO TODO
+            Muscle.LEGS -> DataSource.leg_exercises
+            Muscle.CALVES -> DataSource.calf_exercises
+
+            else -> {DataSource.chest_exercises}
+        }
+
+        return my_list
     }
 }
--- app/src/main/java/com/example/lab3/adapter/ExerciseCardAdapter.kt
+++ app/src/main/java/com/example/lab3/adapter/ExerciseCardAdapter.kt
@@ -1,15 +1,14 @@
 package com.example.lab3.adapter
 
+//import com.example.lab3.const.Layout
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.lab3.R
-//import com.example.lab3.const.Layout
-import com.example.lab3.data.DataSource
+import com.example.lab3.model.Muscle_Exercises
 
 /** TODO change this
  * Adapter to inflate the appropriate list item layout and populate the view with information
@@ -17,49 +16,38 @@ import com.example.lab3.data.DataSource
  */
 class ExerciseCardAdapter(
     private val context: Context?,
-    private val layout: Int
-): RecyclerView.Adapter<ExerciseCardAdapter.DogCardViewHolder>() {
+    private val my_excercises: Muscle_Exercises
+): RecyclerView.Adapter<ExerciseCardAdapter.ExerciseCardViewHolder>() {
 
     //Initialize the data using the List found in data/DataSource TODO
-    val exercises = DataSource.chest_exercises.exercises
-    val type = DataSource.chest_exercises.my_muscle
+    val type = my_excercises.my_muscle
+    val exercises = my_excercises.exercises
 
     /**
      * Initialize view elements TODO
      */
-    class DogCardViewHolder(view: View?): RecyclerView.ViewHolder(view!!) {
-        //Declare and initialiViewById<TextView>(R.id.tree_name)
-        //        var tree_zones = view?.fize all of the list item UI components TODO
-//        var tree_image_id = view?.findViewById<ImageView>(R.id.tree_image)
-//        var tree_name = view?.findndViewById<TextView>(R.id.tree_zones)
-//        var tree_growth_rate = view?.findViewById<TextView>(R.id.tree_growth_rate)
-
+    class ExerciseCardViewHolder(view: View?): RecyclerView.ViewHolder(view!!) {
         var exercise_gif_id = view?.findViewById<pl.droidsonroids.gif.GifImageView>(R.id.exercise_gif)
         var exercise_name = view?.findViewById<TextView>(R.id.exercise_name)
         var exercise_description = view?.findViewById<TextView>(R.id.exercise_description)
-
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DogCardViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExerciseCardViewHolder {
         var layout_view: View? = null
         layout_view = LayoutInflater.from(parent.context).inflate(R.layout.vertical_card, parent, false)
 
-        return DogCardViewHolder(layout_view)
+        return ExerciseCardViewHolder(layout_view)
     }
 
-    override fun getItemCount(): Int = 0 //exercises.size //Return the size of the dataset TODO
+    override fun getItemCount(): Int =  exercises.size //Return the size of the dataset TODO
 
-    override fun onBindViewHolder(holder: DogCardViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ExerciseCardViewHolder, position: Int) {
         //Get the resources from context
         val resources = context?.resources
+
         //For the current tree, apply to the holder the 4 tree variables TODO
         exercises[position].apply {
             holder.apply {
-//                tree_image_id?.setImageResource(imageResourceId)
-//                tree_name?.text = name
-//                tree_zones?.text = resources?.getString(R.string.tree_zones, zones)
-//                tree_growth_rate?.text = resources?.getString(R.string.tree_growth_rate, growth_rate)
-
                 exercise_gif_id?.setImageResource(gifResourceId)
                 exercise_name?.text = name
                 exercise_description?.text = description
